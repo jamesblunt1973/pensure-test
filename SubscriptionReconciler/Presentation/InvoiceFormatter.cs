4747363ebf1b1f5b64ca21c02acbae5addead8fd
Mahdi Malekan <m.malekan@live.com>
2025-12-01 00:01:04 +0100

    Add Presentation layer • Format method takes a ReconciliationResult objects and generates a formatted invoice as a string to show in console

---
+++ SubscriptionReconciler/Presentation/InvoiceFormatter.cs
@@ -0,0 +1,29 @@
+﻿using SubscriptionReconciler.Models;
+using System.Globalization;
+using System.Text;
+
+namespace SubscriptionReconciler.Presentation;
+
+public class InvoiceFormatter
+{
+	public static string Format(ReconciliationResult reconcileRes)
+	{
+		var sb = new StringBuilder();
+
+		string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(reconcileRes.Month);
+		
+		sb.AppendLine("Invoice Breakdown");
+		sb.AppendLine($"For: {monthName}, {reconcileRes.Year}");
+		sb.AppendLine();
+
+		foreach (var line in reconcileRes.InvoiceLines)
+		{
+			sb.AppendLine($"{line.PlanType}: {line.Start:MMM dd} -> {line.End:MMM dd} #{line.Days} day(s) = ${line.AmountRounded}");
+		}
+
+		sb.AppendLine();
+		sb.AppendLine($"Total: ${reconcileRes.TotalRounded:F2}");
+		
+		return sb.ToString();
+	}
+}
