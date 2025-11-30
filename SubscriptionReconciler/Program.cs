1292b19fe6531a83023a4faf342675bc683de87f
Mahdi Malekan <m.malekan@live.com>
2025-12-01 00:03:18 +0100

    Modify Program.cs and add some data to test the service and see a sample output

--- SubscriptionReconciler/Program.cs
+++ SubscriptionReconciler/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SubscriptionReconciler.Application;
+using SubscriptionReconciler.Models;
+using SubscriptionReconciler.Presentation;
+
+SubscriptionReconcilerService service = new(PlanType.Basic);
+List<SubscriptionEvent> events = [
+	new(new DateTime(2024, 4, 5), EventType.Upgrade, Plan.Premium),
+	new(new DateTime(2024, 4, 15), EventType.Downgrade, Plan.Basic),
+	new(new DateTime(2024, 4, 25), EventType.Cancel),
+];
+
+var reconcileResult = service.Reconcile(events, 2024, 4);
+Console.WriteLine(InvoiceFormatter.Format(reconcileResult));
