d60c75683e57fd05dee6fd6f6d854eb72b3765cf
Mahdi Malekan <m.malekan@live.com>
2025-11-30 23:54:16 +0100

    Add Domain Models • EventType & PlanType are enums • Plan has static members for easier access to predefined plans

---
+++ SubscriptionReconciler/Models/InvoiceLine.cs
@@ -0,0 +1,11 @@
+﻿namespace SubscriptionReconciler.Models;
+
+public sealed class InvoiceLine
+{
+	public PlanType PlanType { get; init; }
+	public DateTime Start { get; set; }
+	public DateTime End { get; set; }
+	public int Days { get; init; }
+	public decimal AmountExact { get; init; }
+	public decimal AmountRounded { get; init; }
+}
---
+++ SubscriptionReconciler/Models/Plan.cs
@@ -0,0 +1,16 @@
+﻿namespace SubscriptionReconciler.Models;
+
+public sealed class Plan(PlanType type, decimal monthlyPrice)
+{
+	public PlanType PlanType { get; } = type;
+	public decimal Price { get; } = monthlyPrice;
+
+	public static readonly Plan Basic = new(PlanType.Basic, 10m);
+	public static readonly Plan Premium = new(PlanType.Premium, 30m);
+
+	public static readonly Dictionary<PlanType, Plan> ActivePlans = new()
+		{
+			{ Basic.PlanType, Basic },
+			{ Premium.PlanType, Premium }
+		};
+}
